dfa79fcabb960bcd4c5913ccebb9f2bfc5a46c2e
Chidera-DevOps <deradev24@gmail.com>
2026-03-23 01:12:22 +0000

    Modifications made to system-health-monitor.sh

--- system-health-monitor.sh
+++ system-health-monitor.sh
@@ -2,6 +2,8 @@
 echo 
 echo "===== SYSTEM HEALTH ======"
 
-echo "CPU Usage: $(mpstat 1 1 | awk '/Average/ {printf("%.0f%%\n", 100 - $NF)}')"
-echo "Memory Usage: $(free -m | awk '/Mem:/ {printf("%.0f%%\n", (($2 - $7) / $2) * 100}')"
-:
+CPU="CPU Usage: $(mpstat 3 3 | awk '/Average/ {printf "%.0f%%\n", 100 - $NF}')"
+MEM="Memory Usage: $(free -m | awk '/Mem:/ {printf "%.0f%%\n", (($2 - $7) / $2) * 100}')"
+DISK="Disk Usage: $(df -h / | awk 'NR==2 {print $5}')"
+
+if [[ $CPU== ` ]]
